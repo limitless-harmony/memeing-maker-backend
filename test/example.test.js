e69bda71c4a08a918758eea1022c29b8ac59deed
Audax Anchirinah <audax338@gmail.com>
2019-03-16 08:37:04 +0100

    setup circleCI continuous Integration
    * - Added CircleCi config.yml
    - Added babel support for mocha
    - Added a simple test
    
    * - Fixed CircleCi config error
    
    * - Removed postinstall script
    
    * - Added --exit flag to mocha command
    - Added postinstall back
    
    * - Added CircleCi badge to README file
    
    * -Readded postinstall
    
    * - Fixed CircleCi badgen to point to default
    - Refactored postinstall sequence

--- test/example.test.js
+++ test/example.test.js
@@ -7,7 +7,9 @@ const { expect } = require('chai');
 chai.use(http);
 
 describe('Example title', () => {
-  it('Test case goes here', () => {
-
+  it('Test case goes here', (done) => {
+    const str = 'Hello guys!';
+    expect(str).to.equal('Hello guys!');
+    done();
   });
 });
