daa384119d772efffb740afbe0229c730b7c1f6f
brainyfarm <brainyfarm@gmail.com>
2019-05-23 14:58:03 +0100

    change method to put

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,6 @@ const router = Router();
 
 // TODO: FLESH THIS OUT
 router.get('/:userId', UserController.getUserProfile);
-router.post('/:userId', UserController.updateUserData);
+router.put('/:userId', UserController.updateUserData);
 
 export default router;
